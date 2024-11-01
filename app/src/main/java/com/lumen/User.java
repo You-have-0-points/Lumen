d1a15e4ed69928b11e1d92adc365d4f1f2ef00e4
Muffin5 <muffindop@gmail.com>
2024-11-01 22:30:11 +0300

    add User class

---
+++ app/src/main/java/com/lumen/User.java
@@ -0,0 +1,157 @@
+package com.lumen;
+
+import java.util.ArrayList;
+/*
+public class User {
+    private String name;
+    private String surname;
+    private String license;
+    private String specialization;
+    private String scientificDegree;
+    private int experience;
+    private String university;
+    private String placeOfWork;
+    private int age;
+    private int avatar;
+    private ArrayList<Article> listOfArticles;
+    private ArrayList<Article> favouriteList;
+    private ArrayList<User> subscriberList;
+    private ArrayList<User> subscriptionList;
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void setScientificDegree(String scientificDegree) {
+        this.scientificDegree = scientificDegree;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPlaceOfWork(String placeOfWork) {
+        this.placeOfWork = placeOfWork;
+    }
+
+    public void setAvatar(int avatar) {
+        this.avatar = avatar;
+    }
+
+    public void setListOfArticles(ArrayList<Article> listOfArticles) {
+        this.listOfArticles = listOfArticles;
+    }
+
+    public void setFavouriteList(ArrayList<Article> favouriteList) {
+        this.favouriteList = favouriteList;
+    }
+
+    public void setSubscriberList(ArrayList<User> subscriberList) {
+        this.subscriberList = subscriberList;
+    }
+
+    public void setSubscriptionList(ArrayList<User> subscriptionList) {
+        this.subscriptionList = subscriptionList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public String getScientificDegree() {
+        return scientificDegree;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public String getPlaceOfWork() {
+        return placeOfWork;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public int getAvatar() {
+        return avatar;
+    }
+
+    public ArrayList<Article> getListOfArticles() {
+        return listOfArticles;
+    }
+
+    public ArrayList<Article> getFavouriteList() {
+        return favouriteList;
+    }
+
+    public ArrayList<User> getSubscriberList() {
+        return subscriberList;
+    }
+
+    public ArrayList<User> getSubscriptionList() {
+        return subscriptionList;
+    }
+
+    public void addSubscriber(User user){
+        subscriberList.add(user);
+    }
+
+    public void removeSubscriber(User user){
+        subscriberList.remove(user);
+    }
+
+    public void addSubscription(User user){
+        subscriptionList.add(user);
+    }
+
+    public void removeSubscription(User user){
+        subscriptionList.remove(user);
+    }
+
+    public void addArticle(Article article){
+        listOfArticles.add(article);
+    }
+
+    public void removeArticle(Article article){
+        listOfArticles.remove(article);
+    }
+}
+*/
