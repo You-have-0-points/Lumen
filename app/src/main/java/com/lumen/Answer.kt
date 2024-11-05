32459130f0319f253aa075fd6dc8a1d28a9955ba
Togeekiss <dudarnik2016@gmail.com>
2024-11-05 16:49:39 +0300

    Add class Answer

---
+++ app/src/main/java/com/lumen/Answer.kt
@@ -0,0 +1,52 @@
+package com.lumen
+
+import android.text.format.DateFormat
+//необходимо импортировать класс User
+
+class Answer (
+    private var text: String,
+    private var author: User,
+    private var date: DateFormat,
+    private var likes: ULong,
+    private var isBestAnswer: Boolean,
+    private var messageReply: Answer?
+) {
+    // Геттеры
+    fun getText(): String = text
+
+    fun getAuthor(): User = author
+
+    fun getDate(): DateFormat = date
+
+    fun getLikes(): ULong = likes
+
+    fun isBestAnswer(): Boolean = isBestAnswer
+
+    fun getMessageReply(): Answer? = messageReply
+
+    // Сеттеры
+
+    fun setText(text: String) {
+        this.text = text
+    }
+
+    fun setAuthor(author: User) {
+        this.author = author
+    }
+
+    fun setDate(date: DateFormat) {
+        this.date = date
+    }
+
+    fun setLikes(likes: ULong) {
+        this.likes = likes
+    }
+
+    fun setIsBestAnswer(isBestAnswer: Boolean) {
+        this.isBestAnswer = isBestAnswer
+    }
+
+    fun setMessageReply(messageReply: Answer?) {
+        this.messageReply = messageReply
+    }
+}
